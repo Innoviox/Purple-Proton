80f685e82a66f0308a2f3dcecbbe3cf2e14f2e85
Innoviox <sarras305@gmail.com>
2017-01-16 17:43:49 -0500

    stax finished

--- newnewcpu.py
+++ newnewcpu.py
@@ -1,20 +1,24 @@
 import itertools
 import string
 import collections
-import copy
+import time
 
 leaves = open("leaves.txt").read().split()
 leavesDict = {leaves[i]:float(leaves[i+1]) for i in range(0, len(leaves), 2)}
-leavesDict[''] = 1000
+leavesDict[''] = 0
 
 class Move():
-    def __init__(self, word, board, row, column, direction, prevBoard):
+    def __init__(self, word, board, row, column, direction, prevBoard, \
+                 doNotScoreWord = False, revWordWhenScoring = True):
         self.word = word
         self.board = board
         self.row = row
         self.col = column
         self.direction = direction
         self.prevBoard = prevBoard
+        self.dnsw = doNotScoreWord
+        self.rwws = revWordWhenScoring
+        
     def comp(self, other):
         return self.score+self.valuation>other.score+other.valuation
 
@@ -22,14 +26,14 @@ class Move():
         self.score = self.prevBoard.getScore(self)
         return self.score
     def getEvaluation(self, rack):
-        nR = rack.rack[:]
+        nR = rack[:]
         for letter in self.word:
             try:
                 nR.remove(letter)
             except:
                 pass #through-words & to-words, ??
         self.valuation = leavesDict[''.join(i for i in sorted(nR))]
-
+        return self.valuation
 class Board():
     def __init__(self, board=None):
         if board is None:
@@ -39,9 +43,9 @@ class Board():
                       ['03', ' ', ' ', 'DWS', ' ', ' ', ' ', 'DLS', ' ', 'DLS', ' ', ' ', ' ', 'DWS', ' ', ' '],
                       ['04', 'DLS', ' ', ' ', 'DWS', ' ', ' ', ' ', 'DLS', ' ', ' ', ' ', 'DWS', ' ', ' ', 'DLS'],
                       ['05', ' ', ' ', ' ', ' ', 'DWS', ' ', ' ', ' ', ' ', ' ', 'DWS', ' ', ' ', ' ', ' '],
-                      ['06', ' ', 'TLS', ' ', ' ', ' ', 'TLS', ' ', ' ', ' ', 'Q', ' ', ' ', ' ', 'TLS', ' '],
-                      ['07', ' ', ' ', 'DLS', ' ', ' ', ' ', 'DLS', ' ', 'DLS', 'I', ' ', ' ', 'DLS', ' ', ' '],
-                      ['08', 'TWS', ' ', ' ', 'DLS', ' ', ' ', 'B', 'O', 'G', 'S', ' ', 'DLS', ' ', ' ', 'TWS'],
+                      ['06', ' ', 'TLS', ' ', ' ', ' ', 'TLS', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'TLS', ' '],
+                      ['07', ' ', ' ', 'DLS', ' ', ' ', ' ', 'DLS', ' ', 'DLS', ' ', ' ', ' ', 'DLS', ' ', ' '],
+                      ['08', 'TWS', ' ', ' ', 'DLS', ' ', ' ', 'B', 'A', 'G', ' ', ' ', 'DLS', ' ', ' ', 'TWS'],
                       ['09', ' ', ' ', 'DLS', ' ', ' ', ' ', 'DLS', ' ', 'DLS', ' ', ' ', ' ', 'DLS', ' ', ' '],
                       ['10', ' ', 'TLS', ' ', ' ', ' ', 'TLS', ' ', ' ', ' ', 'TLS', ' ', ' ', ' ', 'TLS', ' '],
                       ['11', ' ', ' ', ' ', ' ', 'DWS', ' ', ' ', ' ', ' ', ' ', 'DWS', ' ', ' ', ' ', ' '],
@@ -81,7 +85,9 @@ class Board():
                 if col not in self.extraList:
                     nR, nC = rIndex, cIndex
                     word=collections.OrderedDict()
-                    while uai.count((nR, nC))<1 and nC < len(board[nR]) and board[nR][nC] not in self.extraList:
+                    while uai.count((nR, nC))<1 and \
+                          nR < len(board) and nC < len(board[nR]) and \
+                          board[nR][nC] not in self.extraList:
                         letter = board[nR][nC]
                         if word.get(letter):
                             letter += str(c) #differentiate
@@ -97,7 +103,9 @@ class Board():
                     
                     nC = cIndex #reset horizontal index
                     word=collections.OrderedDict()
-                    while udi.count((nR, nC))<1 and board[nR][nC] not in self.extraList:
+                    while udi.count((nR, nC))<1 and \
+                          nR < len(board) and nC < len(board[nR]) and \
+                          board[nR][nC] not in self.extraList:
                         letter = board[nR][nC]
                         if word.get(letter):
                             letter += str(c) #differentiate
@@ -115,7 +123,9 @@ class Board():
 
     def expandFrom(self, point, places, extendedFrom):
         assert point not in extendedFrom
+        
         usedPlaces = [point]
+        
         rT, cT = point
         cT += 1
         while (rT, cT) in places:
@@ -134,7 +144,7 @@ class Board():
             usedPlaces.append((rT, cT))
             rT += 1
 
-        rT = point[1]
+        rT = point[0]
         rT -= 1
         while (rT, cT) in places:
             usedPlaces.append((rT, cT))
@@ -169,7 +179,8 @@ class Board():
         usedPlaces = self.expandFrom(top, places, extendedFrom)
         l = [coord for place in places for coord in place]
         #Extend to the edge of the board
-        for i in range(min(l), max(l)+1):
+        #for i in range(min(l), max(l)+1):
+        for j in range(15):
             np = usedPlaces[:]
             for i in np:
                 try:
@@ -206,27 +217,34 @@ class Board():
         wordScore = 0
         wordMult = 1
         scored = []
-        for (index, letter) in enumerate(reversed(move.word)):
-            row = move.row
-            col = move.col
-            
-            if move.direction == 'D':
-                row -= index
+        if not move.dnsw:
+            if move.rwws:
+                sw = reversed(move.word)
+                im = 1
             else:
-                col -= index
+                sw = move.word
+                im = -1
+            for (index, letter) in enumerate(sw):
+                row = move.row
+                col = move.col
                 
-            lettMult = 1
-            oldLetter = self.board[row][col]
-            if oldLetter in ['TLS', 'DLS']:
-                lettMult *= ['D', 'T'].index(oldLetter[0])+2
-            elif oldLetter in ['TWS', 'DWS']:
-                wordMult *= ['D', 'T'].index(oldLetter[0])+2
-            elif oldLetter == '*':
-                wordMult *= 2
-            wordScore += self.scores[letter.lower()] * lettMult
+                if move.direction == 'D':
+                    row -= index*im
+                else:
+                    col -= index*im
+                    
+                lettMult = 1
+                oldLetter = self.board[row][col]
+                if oldLetter in ['TLS', 'DLS']:
+                    lettMult *= ['D', 'T'].index(oldLetter[0])+2
+                elif oldLetter in ['TWS', 'DWS']:
+                    wordMult *= ['D', 'T'].index(oldLetter[0])+2
+                elif oldLetter == '*':
+                    wordMult *= 2
+                wordScore += self.scores[letter.lower()] * lettMult
 
-            scored.append((row, col))
-        wordScore *= wordMult
+                scored.append((row, col))
+            wordScore *= wordMult
 
         for word in newWords:
             if ''.join(letter[0] for letter in word.keys()) != move.word: #fix for same auxillary words
@@ -244,6 +262,8 @@ class Board():
                     auxWordScore += self.scores[letter.lower()] * lettMult
                 auxWordScore *= auxWordMult
                 wordScore += auxWordScore
+        if len(move.word) == 7:
+            wordScore += 50 #Bingo!
         return wordScore
                     
         
@@ -311,23 +331,45 @@ class CPU():
             for i in range(1, 15):
                 places.append((i, 8))
                 places.append((8, i))
+        across, down = [], []
         for place in places:
-            rIndex, cIndex = place
-            neighbors.extend(itertools.starmap(lambda a,b: (rIndex+a, cIndex+b), \
-                                          itertools.product((0,-1,+1), (0,-1,+1))))
+            r, c = place
+            neighbors.append((r+1,c))
+            neighbors.append((r-1,c))
+            neighbors.append((r,c+1))
+            neighbors.append((r,c-1))
         neighbors = self.board.removeDuplicates(neighbors)
         for word in words:
             for neighbor in neighbors:
                 rIndex, cIndex = neighbor
                 for direc in ['A', 'D']:
-                    #print(rIndex, cIndex, direc)
                     newBoard = self.rNab()
                     if self.playWord(word, rIndex, cIndex, direc, newBoard):
                         play = Move(word, newBoard, rIndex, cIndex, direc, prevBoard)
-                        self.displayBoard(play.board.board)
-                        #self.displayBoard(self.board.board)
-                        print(play.getScore())
+                        play.getScore()
+                        play.getEvaluation(self.rack)
                         plays.append(play)
+                        continue
+                        
+                    newBoard = self.rNab()
+                    if self.playWordOpp(word, rIndex, cIndex, direc, newBoard):
+                        play = Move(word, newBoard, rIndex, cIndex, direc, prevBoard, revWordWhenScoring=False)
+                        play.getScore()
+                        play.getEvaluation(self.rack)
+                        plays.append(play)
+                print(word, neighbor, "done", len(plays))
+
+        for (d, row) in enumerate(self.board.board[1:]):
+            for play in self.complete(self.slotify(row[1:]), 'A', d+1):
+                play.getScore()
+                play.getEvaluation(self.rack)
+                plays.append(play)
+        columns = [[row[i] for row in self.board.board[1:]] for i in range(len(self.board.board))]
+        for (d, col) in enumerate(columns):
+            for play in self.complete(self.slotify(col), 'D', d):
+                play.getScore()
+                play.getEvaluation(self.rack)
+                plays.append(play)               
         return plays
 
     def proxyBoard(self):
@@ -335,6 +377,8 @@ class CPU():
 
     def playWord(self, word, row, col, direc, board):
         for letter in reversed(word):
+            if row>15 or col>15:
+                return False
             if board.board[row][col] in string.ascii_uppercase:
                 return False
             board.board[row][col] = letter
@@ -346,6 +390,21 @@ class CPU():
             return True
         return False
 
+    def playWordOpp(self, word, row, col, direc, board):
+        for letter in word:
+            if row>15 or col>15:
+                return False
+            if board.board[row][col] in string.ascii_uppercase:
+                return False
+            board.board[row][col] = letter
+            if direc=='A':
+                col += 1
+            else:
+                row += 1
+        if board.checkBoard(board.board):
+            return True
+        return False
+    
     def rNab(self):
         nbo = []
         for row in self.board.board:
@@ -353,9 +412,92 @@ class CPU():
             for col in row:
                 nbo[-1].append(col)
         return Board(nbo)
-#b = Board()
-#print(b.getWords(b.board))
-c = CPU([i for i in 'SATIREN'])
+
+    def gac(self, iterable, maxDepth):
+        allWords = []
+        for depth in range(1, maxDepth + 1): 
+            for word in itertools.permutations(iterable, depth):
+                allWords.append("".join(word))
+        return allWords
+    
+    def place(self, slot, pos, word, direc, depth):
+        if depth == 0:
+            return False
+        slot, reps = slot
+        if pos + len(word) > len(slot):
+            return False
+        if slot == '...............':
+            return False
+        currPos = pos
+        newSlot = list(slot)
+
+        index = 0
+        while index < len(word):
+            newPos = currPos + index
+            if newSlot[newPos] != '.':
+                currPos += 1
+                index -= 1
+            else:
+                newSlot[newPos] = word[index]
+                try: wordPos
+                except: wordPos = currPos + index
+            index += 1
+        try: wordPos += 1
+        except: return False
+        newSlot = ''.join(letter for letter in newSlot)
+        if not all(self.checkWord(i) for i in newSlot.strip('.').split('.') if i != ''):
+            return False
+        print(newSlot)
+        newBoardSlot = []
+        for (index, newLetter) in enumerate(newSlot):
+            if newLetter == '.':
+                newBoardSlot.append(reps[index])
+            else:
+                newBoardSlot.append(newLetter)
+        newBoard = self.rNab()
+        oldBoard = self.rNab()
+        row, col = depth, depth
+        if direc == 'A':
+            newBoardSlot.insert(0, str(depth).zfill(2))
+            newBoard.board[depth] = newBoardSlot[:]
+            col = wordPos
+        else:
+            for (index, row) in enumerate(newBoard.board[1:]):
+                row[depth] = newBoardSlot[index]
+
+            row = wordPos
+        move = Move(word, newBoard, row, col, direc, oldBoard, doNotScoreWord=True)
+        return move
+    
+    def complete(self, slot, direc, depth):
+        words = self.board.removeDuplicates(self.gac(self.rack, 7))
+        newSlots = []
+        slotForLen = slot[0]
+        if slotForLen != '...............':
+            edgeFinder = [i[0] for i in enumerate(slotForLen) if i[1] !='.']
+            for word in words:
+                for pos in range(edgeFinder[0], edgeFinder[-1]+len(word)+2):
+                    if pos-len(word) in range(len(slotForLen)):
+                        if slotForLen[pos-len(word)] == '.':
+                            newSlot = self.place(slot, pos-len(word), word, direc, depth)
+                            if newSlot:
+                                newSlots.append(newSlot)
+        return newSlots
+
+    def slotify(self, slot):
+        slotForReps = slot
+        slot = ''.join(i for i in slot)
+        slot = slot.replace(' ', '.')
+        for i in self.extraList:
+            slot = slot.replace(i, '.')
+        print(slot)
+        return slot, slotForReps
+    
+c = CPU([i for i in 'ASW'])
 for i in c.takeTurn():
-    pass
-    #c.displayBoard(i[-1].board)
+    c.displayBoard(i.board.board)
+    print(i.score, i.valuation)
+    print(i.row, i.col)
+    print(i.board.board[i.row])
+    print(i.board.board[i.row][i.col])
+    print(i.dnsw, i.rwws)
